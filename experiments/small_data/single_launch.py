f6de6d721167e503acdde93e45a7c568467aa3c4
Ties Robroek <tt_robroek@hotmail.com>
2018-10-22 10:09:14 +0200

    fixed single_launch

--- experiments/small_data/single_launch.py
+++ experiments/small_data/single_launch.py
@@ -9,7 +9,7 @@ for method in methods[:1]:
     for dkey in params.datasets.keys()[:1]:
         for train_size in params.datasets[dkey]['train_sizes'][:1]:
             for seed in seeds[:1]:
-                for key in params.parameters[:1]:
+                for key in params.parameters:
                     print("Processing method %s with data set %s, train_size %s, seed %s, and key %s ..." % (str(method), str(dkey), str(train_size), str(seed), str(key)))
                     cmd = "python " + method + ".py --dkey %s --train_size %i --seed %i --key %s" % (dkey, train_size, seed, key)
                     print(cmd)
