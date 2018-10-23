3f1b0e2e1fd2c328a3083d717f8e53b86c13b3b6
Sipondo <tt_robroek@hotmail.com>
2018-10-23 11:14:47 +0200

    fix error reporting

--- woody/__init__.py
+++ woody/__init__.py
@@ -3,18 +3,18 @@
 # License: GPL v2
 #
 
-""" 
+"""
 The woody package aims at large-scale implementations
-for random forests. It is based on an efficient C 
-implementation  and resorts to distributed computing 
+for random forests. It is based on an efficient C
+implementation  and resorts to distributed computing
 strategies.
 """
-
-import warnings
-
-try:
-    from woody.models import WoodClassifier, WoodRegressor, HugeWoodClassifier, HugeWoodRegressor, SubsetWoodClassifier, SubsetWoodRegressor
-except Exception as e:
-    warnings.warn("Swig models not compiled yet? Error message: %s" % str(e))
+# 
+# import warnings
+#
+# try:
+#     from woody.models import WoodClassifier, WoodRegressor, HugeWoodClassifier, HugeWoodRegressor, SubsetWoodClassifier, SubsetWoodRegressor
+# except Exception as e:
+#     warnings.warn("Swig models not compiled yet? Error message: %s" % str(e))
 
 __version__ = "0.3.1"
