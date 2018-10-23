491dc75c737629826a7b7a57186620892f98a3bd
Sipondo <tt_robroek@hotmail.com>
2018-10-23 12:42:03 +0200

    commented

--- woody/models/forest/base.py
+++ woody/models/forest/base.py
@@ -14,7 +14,7 @@ from woody.util.array import transpose_array
 from woody.util import draw_single_tree
 
 class Wood(object):
-    from .treesolver_python import treesolve
+    # from .treesolver_python import treesolve
     """
     Random forest implementation.
     """
